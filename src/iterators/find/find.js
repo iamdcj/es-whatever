780587bbe80f3bcd7d8d511dc7491081d8fb27f5
David Christian Jones <david@dcj.codes>
2020-02-08 12:10:23 -0500

    add JS

--- src/iterators/find/find.js
+++ src/iterators/find/find.js
@@ -1,5 +1,34 @@
-// Integers
+// Find
+[1, 2, 3].find(n => n === 2); // 2
 
-// Strings
+// No Find
+[1, 2, 3].find(n => n === 4); // undefined
 
 // Objects
+
+const users = [
+  { user: "David" },
+  { user: "Steffie" },
+  { user: "Nole", id: 1 },
+  { user: "Nole", id: 2 }
+];
+
+users.find(({ user }) => user === "Nole");
+
+// {user: "Nole", id: 1}
+
+// Number
+
+const ints = [3, 12, 200, 101, 230, 30];
+
+ints.find(i => i > 100);
+
+// 200;
+
+//String
+
+const names = ["David", "Steffie", "Nole"];
+
+names.find(n => n.includes("D"));
+
+// "David"
