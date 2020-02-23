daf4380fc81b7eecd495d2684e9cbf2e6466bddc
David Christian Jones <david@dcj.codes>
2020-02-23 10:41:02 -0500

    named example (nodejs)

---
+++ src/modules/examples/named/index.mjs
@@ -0,0 +1,4 @@
+import { sum, adder } from "./modules.mjs";
+
+console.log(sum(12, 12)); // 144
+console.log(adder(10, 9)); // 19
---
+++ src/modules/examples/named/modules.mjs
@@ -0,0 +1,3 @@
+export const sum = (x, y) => x * y;
+
+export const adder = (x, y) => x + y;
